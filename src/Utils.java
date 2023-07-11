f7acc12d54d2288cac26deef8332e9c6b3f05045
ehsanhosseini1380 <ehosseini8001@gmail.com>
2023-07-11 12:29:51 +0330

    errors color changed to red

--- src/Utils.java
+++ src/Utils.java
@@ -13,7 +13,7 @@ public class Utils {
 
     public static void detectUndeclaredVariable(TerminalNode id, Stack<SymbolTable> scopes){
         if (deepLookup(String.format("Field_%s",id.getText()),scopes) == null)
-            System.out.printf("Error100 : in line [%d:%d], Can not find Variable [%s]\n", id.getSymbol().getLine(), id.getSymbol().getCharPositionInLine() + 1, id.getText());
+            System.err.printf("Error100 : in line [%d:%d], Can not find Variable [%s]\n", id.getSymbol().getLine(), id.getSymbol().getCharPositionInLine() + 1, id.getText());
     }
 
 
@@ -21,7 +21,7 @@ public class Utils {
         if (scopes.peek().lookup(fieldType + "_" + identifier)!=null) {
             fieldType = fieldType.toLowerCase();
             int errorNo = (fieldType.equals("field")) ? 104 : 102;
-            System.out.printf("Error%d : in line [%d:%d], %s [%s] has been defined already\n", errorNo, line, column, fieldType, identifier);
+            System.err.printf("Error%d : in line [%d:%d], %s [%s] has been defined already\n", errorNo, line, column, fieldType, identifier);
 
             return true;
         }
@@ -31,18 +31,18 @@ public class Utils {
 
     public static void detectUndeclaredClass(TerminalNode className){
         if(!Boolean.parseBoolean(checkDataTypeIsDefined(className.getText()))){
-            System.out.printf("Error101 : in line [%d:%d], Can not find Class [%s]\n", className.getSymbol().getLine(), className.getSymbol().getCharPositionInLine()+1, className.getText());
+            System.err.printf("Error101 : in line [%d:%d], Can not find Class [%s]\n", className.getSymbol().getLine(), className.getSymbol().getCharPositionInLine()+1, className.getText());
         }
     }
 
 
     public static void reportDuplicateClassError(String identifier, int line, int column){
-        System.out.printf("Error103 : in line [%d:%d], class [%s] has been defined already\n", line, column, identifier);
+        System.err.printf("Error103 : in line [%d:%d], class [%s] has been defined already\n", line, column, identifier);
     }
 
     public static void detectConstructorError(String className, String constructorName, int line, int column){
         if (!Objects.equals(className, constructorName)){
-            System.out.printf("Error105 : in line [%d:%d], constructor name mismatch\n", line, column);
+            System.err.printf("Error105 : in line [%d:%d], constructor name mismatch\n", line, column);
         }
     }
 
@@ -52,7 +52,7 @@ public class Utils {
         int indexValue = Integer.parseInt(integer.getText());
 
         if (indexValue >= arraySize ) {
-            System.out.printf("Error106 : in line [%d:%d], array index out of range\n", line, column);
+            System.err.printf("Error106 : in line [%d:%d], array index out of range\n", line, column);
         }
     }
 
@@ -65,11 +65,11 @@ public class Utils {
                 for(int i = 0; i < ctx.exp().size(); i++){
                     HashMap<String,String> paramProperties=deepLookup(String.format("Field_%s",ctx.exp(i).getText()),scopes);
                     if(!paramProperties.get("type").equals(methodProperties.get(String.format("type%d",i+1)))) {
-                        System.out.printf("Error107 : in line [%d], method parameter mismatch\n", line);
+                        System.err.printf("Error107 : in line [%d], method parameter mismatch\n", line);
                     }
                 }
             }
-            else{ System.out.printf("Error108 : in line [%d], this method expects %d parameters\n", line, Integer.parseInt(methodProperties.get("paramCount")));
+            else{ System.err.printf("Error108 : in line [%d], this method expects %d parameters\n", line, Integer.parseInt(methodProperties.get("paramCount")));
             }
         }
     }
